c25b303af3185292e7de9e16a2d92164a037c2ba
John <jbertlik@gmail.com>
2019-05-20 20:36:04 -0400

    downloaded and installed Postgres on my machine and changed the syntax in this file in order to manipulate the data via Postgres.

--- database_test.py
+++ database_test.py
@@ -1,23 +1,23 @@
-import sqlite3
+# import psycopg2
+import psycopg2
 
 def create_table():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item,quantity,price):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("INSERT INTO store VALUES (?,?,?)",(item,quantity,price))
+            # (item,quantity,price))
+    cur.execute("INSERT INTO store VALUES (%s,%s,%s)", (item,quantity,price))
     conn.commit()
     conn.close()
 
-
-
 def view():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
@@ -25,21 +25,30 @@ def view():
     return rows
 
 def delete(item):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,)) # for SQLite3 use '?' instead of '%s'
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close()
 
 
-update(11,6,"Water Glass")
+# Postgres
+create_table()
+# insert("Orange",10,15)
+# delete("Orange")
+update(20,15,'Apple')
+print(view())
+
+
+# SQLite3
+# update(11,6,"Water Glass")
 # delete("Coffee Cup")
 # insert("Coffee Cup",10,5)
-print(view())
+# print(view())
